a086099758b7aa0dbf88eb2c0bf5a2d76b036ce0
workman <samuel.workman@tomtom.com>
2022-07-20 09:54:21 -0400

    its a guessing game

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def guess_code():
+    val = input("Enter your value: ")
+    print(generate_unique_code())
 
 
-# Press the green button in the gutter to run the script.
+def generate_unique_code():
+    # im using integers in variable names and one can stop me.
+    code1 = random.randrange(9)
+    code2 = random.randrange(9)
+    code3 = random.randrange(9)
+    code4 = random.randrange(9)
+
+    # for sure not the right way to do this, but hackathon
+    while code1 == code2 or code1 == code3 or code1 == code4:
+        code1 = random.randrange(9)
+
+    while code2 == code1 or code2 == code3 or code2 == code4:
+        code2 = random.randrange(9)
+
+    while code3 == code1 or code3 == code2 or code3 == code4:
+        code3 = random.randrange(9)
+
+    return str(code1) + str(code2) + str(code3) + str(code4)
+
+
+def generate_duplicate_code():
+    # im using integers in variable names and one can stop me.
+    code1 = random.randrange(9)
+    code2 = random.randrange(9)
+    code3 = random.randrange(9)
+    code4 = random.randrange(9)
+
+    return str(code1) + str(code2) + str(code3) + str(code4)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    guess_code()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
